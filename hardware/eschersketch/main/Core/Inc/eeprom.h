468845c8d626bfff37796ad13a1857250a791f26
Michael Dewberry <michael.dewberry@gmail>
2022-11-13 23:46:26 -0500

    Get Escher Sketch building with PlatformIO

--- hardware/eschersketch/main/Core/Inc/eeprom.h
+++ hardware/eschersketch/main/Core/Inc/eeprom.h
@@ -45,7 +45,7 @@
 /* Includes ------------------------------------------------------------------*/
 
 #include "stm32f4xx_hal.h"
-#include "stm32_hal_legacy.h"
+//#include "stm32_hal_legacy.h"
 
 /* Exported constants --------------------------------------------------------*/
 /* EEPROM emulation firmware error codes */
